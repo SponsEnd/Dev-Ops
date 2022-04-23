8065d5e436c39e3182c6f722fa369bd7e646334f
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2022-04-23 15:04:58 +0530

    Added test route

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import request
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
 @app.route("/sendRequest")
 def send_request():
     transcript = request.args.get("id")
